bcb82a3eabe758c95f51b6a71c0cda479155cec0
Estudiante <Estudiante@10.203.3.127>
2018-04-03 08:26:43 -0500

    Ejercicio de herencia

---
+++ src/agregacion/Persona.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agregacion;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Persona {
+    protected String nombre;
+    protected String apellido;
+    protected int edad;
+    private Persona casadoCon;
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+
+    public int getEdad() {
+        return edad;
+    }
+    
+    public void setCasadoCon(Persona casadoCon){
+        this.casadoCon = casadoCon;
+    }
+    
+    public static int diferenciaEdad(Persona persona1,Persona persona2){
+        return Math.abs(persona1.getEdad()-persona2.getEdad());
+    }
+
+  
+    
+}
