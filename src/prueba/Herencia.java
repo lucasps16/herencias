d9452cbdbb5cf3287f6d37595d5a3f8538863a8e
Estudiante <Estudiante@10.203.3.127>
2018-04-03 08:42:39 -0500

    Ejercicio de herencia

---
+++ src/prueba/Herencia.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+import agregacion.*;
+/**
+ *
+ * @author Estudiante
+ */
+public class Herencia {
+
+   
+    public static void main(String[] args) {
+        Mujer mujer1 = new Mujer("Sofia", "Alvarez", 19);
+        Mujer mujer2 = new Mujer("Camila", "Gomez", 20);
+        Hombre hombre = new Hombre("D", "S", 25);
+        
+        hombre.setCasadoCon(mujer1);
+        
+        hoombre  
+    }
+    
+}
